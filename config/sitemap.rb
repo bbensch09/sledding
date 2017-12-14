9adda1f9821c1177ca7a2c1424fbf5a1b4f1851a
Brian <bbensch@gmail.com>
2017-12-13 22:57:57 -0800

    change mailer url option to domain ENV variable

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://www.snowschoolers.com"
+SitemapGenerator::Sitemap.default_host = "http://lessons.granlibakken.com"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
