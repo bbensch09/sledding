fd301f749f7636a332a2ca9d08fb3ac45787f994
Brian <bbensch@gmail.com>
2017-12-15 08:42:34 -0800

    ski confirmation and allow new user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,15 @@ def after_sign_in_path_for(resource)
   session[:previous_url] || root_path
 end
 
+def after_sign_up_path_for(resource)
+  session[:previous_url] || root_path
+end
+
+def after_confirmation_path_for(resource)
+  session[:previous_url] || root_path
+end
+
+
 def set_user
   if current_user
     cookies[:userId] = current_user.id
