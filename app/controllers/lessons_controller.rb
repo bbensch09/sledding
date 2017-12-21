f957a52201ba914f1e76cc00ab8e681a65543417
Brian <bbensch@gmail.com>
2017-12-21 13:09:24 -0800

    fix admin issue to see lessons schedule

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -90,8 +90,8 @@ class LessonsController < ApplicationController
     if current_user && current_user.email == 'brian@snowschoolers.com' || current_user.user_type == 'Ski Area Partner' || current_user.user_type == "Granlibakken Employee"
       all_days = Section.select(:date).uniq.sort{|a,b| a.date <=> b.date}
       @days = all_days.keep_if{|a| a.date >= Date.today}
-      @days = @days.first(10)
-      # Need to remove restriction after 100 lessons are booked
+      @days = @days.first(30)
+      @new_date = Section.new
       @lessons = Lesson.first(100).to_a.keep_if{|lesson| lesson.completed? || lesson.completable? || lesson.confirmable? || lesson.confirmed?}
       @lessons.sort! { |a,b| a.lesson_time.date <=> b.lesson_time.date }
       if session[:notice]
