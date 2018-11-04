208877ed112543d9c9c511dbc32ddbac62df6207
bbensch09 <bbensch@gmail.com>
2018-11-04 12:02:23 -0800

    change domain to sledding.granlibakken.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "lessons.granlibakken.com",
+    domain: "sledding.granlibakken.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "brian@snowschoolers.com",
