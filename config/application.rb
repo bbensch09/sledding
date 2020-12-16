4a1bde2a7b6d75871e128f867c9d50cd355de73e
Brian Bensch <bbensch@gmail.com>
2020-12-16 08:29:29 -0500

    keep config.assets.initialize_on_precompile as false

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module SnowSchoolers
     # config.i18n.default_locale = :de
 
     # troubleshooting Heroku deploy failures - 11.15.20
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
 
 
   #LOAD all files in library
